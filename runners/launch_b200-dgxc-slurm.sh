dba84c84cea23335f29632400b5bc4f8fc835420
Cam Quilici <cjquilici@gmail.com>
2026-04-23 22:53:58 -0500

    change path

--- runners/launch_b200-dgxc-slurm.sh
+++ runners/launch_b200-dgxc-slurm.sh
@@ -249,7 +249,7 @@ EOF
 
 else
 
-    HF_HUB_CACHE_MOUNT="/scratch/fsw/models"
+    HF_HUB_CACHE_MOUNT="/scratch/fsw/gharunners/hf-hub-cache"
     export MODEL="$HF_HUB_CACHE_MOUNT/${MODEL#*/}"
     SQUASH_FILE="/home/sa-shared/containers/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
     FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
